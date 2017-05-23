36f724988203abc68764d5559405b6b027fd7edb
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-05-23 16:18:19 -0400

    setting up

---
+++ bank.rb
@@ -0,0 +1,23 @@
+class BankAccount
+  @@interest_rate = 0.5
+  @@accounts = []
+
+  attr_accessor :balance
+  attr_reader :tax
+
+  def initialize(name, price)
+   @balance = 0
+ end
+
+  @deposit = 0 + @@accounts
+  @withdraw = 0 - @@accounts
+
+  def self.create(BankAccount.new)
+    @@accounts
+  end
+  return BankAccount.new
+
+
+
+
+end
